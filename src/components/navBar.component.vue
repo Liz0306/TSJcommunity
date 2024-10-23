179e3a76fa14591f59ae41bebcd296836b308f01
Elizabeth Perez <elizabetharriaga007@gmail.com>
2024-10-23 15:26:12 -0600

    commit del main

---
+++ src/components/navBar.component.vue
@@ -0,0 +1,16 @@
+<template>
+   <nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Default</a>
+    <div class="d-flex justify-content-end">
+     
+      
+    </div>
+  </div>
+</nav>
+</template>
+<script>
+export default{
+    name:'NavBarComponent',
+};
+</script>
